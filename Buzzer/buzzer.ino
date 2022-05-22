fb5677a7aa9f437b1117ddae281d9ffc96202f38
MaruthuPandian K <33753644+tinkerermaruthu@users.noreply.github.com>
2022-05-22 11:37:10 +0530

    Create buzzer.ino

---
+++ Buzzer/buzzer.ino
@@ -0,0 +1,27 @@
+
+#include <DHT.h>
+#include <DHT_U.h>
+
+int IRSensor = 8; // connect ir sensor to arduino pin 2
+int Buzzer = 7; // connect Led to arduino pin 13
+
+void setup()
+{
+pinMode (IRSensor, INPUT); // sensor pin INPUT
+pinMode (Buzzer, OUTPUT); // Led pin OUTPUT
+Serial.begin(9600);
+}
+
+void loop()
+{
+if(digitalRead(IRSensor)==HIGH)
+
+tone(Buzzer,500);
+
+
+
+else{
+noTone(Buzzer);
+}
+
+}
