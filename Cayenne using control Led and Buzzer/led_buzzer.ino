9a1cc5d180908cc05c3c90bf226008fd64b2c51d
MaruthuPandian K <33753644+tinkerermaruthu@users.noreply.github.com>
2022-08-25 00:36:03 -0700

    Update led_buzzer.ino

--- Cayenne using control Led and Buzzer/led_buzzer.ino
+++ Cayenne using control Led and Buzzer/led_buzzer.ino
@@ -4,7 +4,7 @@ const char* ssid     = "RACK";         // The SSID (name) of the Wi-Fi network y
 const char* password = "Termux007";     // The password of the Wi-Fi network
 
 char mqtt_username[] = "74b90020-c522-11e6-bd19-2daeb3a13a46";
-
+ 
 char mqtt_password[] = "497c4e2688416a2b887b4cea229e7c5fd91c3730";
 char client_id[] = "8307ca30-d9d5-11ec-8da3-474359af83d7";
 int led=5;
